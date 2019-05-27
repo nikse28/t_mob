f629abb132c9f14860feb7b8455565cc7cde40e7
Nikhil <nikhil.sonwane@opusconsulting.com>
2019-05-27 19:57:16 +0530

    ant d adding and try to implement

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { Row, Col } from "antd";
-import { Tabs } from "antd";
+import { Row, Col ,Input,Tabs,Form} from "antd";
 import Dropdown from "./Dropdown"
 const MainComponent = ()=>{
     const TabPane = Tabs.TabPane;
@@ -10,9 +9,15 @@ const MainComponent = ()=>{
         <Col span={2}>
         </Col>
         <Col span={6}>
+
         <Tabs  type="card">
             <TabPane tab="Tab 1" key="1">
-                <Dropdown/>
+                <Form>
+                <Form.Item label="Source">
+                    <Dropdown/>
+                </Form.Item>
+                
+                </Form>
             </TabPane>
             <TabPane tab="Tab 2" key="2">
               Content of Tab Pane 2
